de4ddcedfb9c665e16be32aa102f478e7759d5fe
cdgalvis <cristiangalviscardona@gmail.com>
2024-05-23 01:46:09 +0200

    Se creo el boton eliminar

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -1,15 +1,27 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import pool from '../database.js'
 
 const router = Router();
 
-router.get('/list', async(req, res) => {
+router.get('/list', async (req, res) => {
     try {
         const [result] = await pool.query('SELECT * FROM personas');
-        res.render('personas/list', {personas: result})
+        res.render('personas/list', { personas: result })
     } catch (error) {
-        res.status(500).json({message: error.message});
+        res.status(500).json({ message: error.message });
     }
 });
+
+router.get('/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        await pool.query('DELETE FROM personas WHERE id = ?', [id]);
+        res.redirect('/list');
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
 
 export default router;
