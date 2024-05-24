3a24b4bed29b820cdc31368a8471bbe38a99d349
cdgalvis <cristiangalviscardona@gmail.com>
2024-05-24 02:21:02 +0200

    Se creo modulo de editar

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -40,4 +40,36 @@ router.get('/delete/:id', async (req, res) => {
 });
 
 
+router.get('/edit/:id', async (req, res)=>{
+    try {
+        const {id} = req.params
+        const [persona] = await pool.query('SELECT * FROM personas WHERE id = ?', [id]);
+        const personaEdit = persona[0]
+        res.render('personas/edit', { persona: personaEdit })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.post('/edit/:id', async (req, res)=>{
+    try {
+        const {id} = req.params
+        const {name, lastname, age}  = req.body
+        const editPersona = {
+                                name, 
+                                lastname, 
+                                age
+                            }
+    
+        await pool.query('UPDATE personas SET ? WHERE id = ?', [editPersona, id]);
+        res.redirect('/list');
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+
+
 export default router;
